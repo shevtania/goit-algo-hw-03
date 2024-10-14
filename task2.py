bbd14843d17d28f818a927f0dbefc093ea29dd53
Tetiana Shevtsova <shevtania@outlook.com>
2024-10-14 21:46:28 +0300

    corrected version

--- task2.py
+++ task2.py
@@ -5,11 +5,8 @@ def get_numbers_ticket(min, max, quantity):
     result = []
      
     if min < 1 or max > 1000  or (min > max): # verification max and min numbers 
-        return f"Wrong max or min numbers. {result}"
-    
-    elif quantity >= max or quantity <= min:  # verification quantity in interval (min, max)
-        return f"Wrong quantity. {result}"
-    
+        return result
+     
 # if upper condition is true, add the integer from the intrrval (min, max + 1)  in set until the length of the set is  become equal to  quantity.
 # After that set is turned into list, and list is sorted
     else:
