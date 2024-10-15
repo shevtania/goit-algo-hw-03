5fe0964c76f67712c02dc02b776b82843d9b6d7c
Tetiana Shevtsova <shevtania@outlook.com>
2024-10-15 18:00:03 +0300

    correction of random numbers

--- task2.py
+++ task2.py
@@ -1,17 +1,17 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    seq = set()  
+    seq = set()  #sequence for saving numbers for uniq values
     result = []
      
     if min < 1 or max > 1000  or (min > max): # verification max and min numbers 
         return result
      
-# if upper condition is true, add the integer from the intrrval (min, max + 1)  in set until the length of the set is  become equal to  quantity.
+# if upper condition is true, add the integer from the intrrval (min, max)  in set until the length of the set is  become equal to  quantity.
 # After that set is turned into list, and list is sorted
     else:
         while len(seq) < quantity:
-            num = random.randint(min, max + 1)
+            num = random.randint(min, max)
             seq.add(num)
         
         result = sorted(list(seq))
