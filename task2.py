f85fa0d3fab08b372689d076b827e7ddaaa7d17e
Tetiana Shevtsova <shevtania@outlook.com>
2024-10-16 23:11:44 +0300

    add check

--- task2.py
+++ task2.py
@@ -2,19 +2,23 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     seq = set()  #sequence for saving numbers for unique values
-    result = []
+    result = [] 
      
     if min < 1 or max > 1000  or (min > max): # verification max and min numbers 
         return result
+    
+    elif max + 1 - min < quantity:  # numbers must be unique, max - min = quantity - 1
+        return result
      
-# if upper condition is true, add the integer from the intrrval (min, max)  in set until the length of the set is  become equal to  quantity.
+# if upper condition is true, add the integer from the interval (min, max)  in set until the length of the set is  become equal to  quantity.
 # After that set is turned into list, and list is sorted
     else:
         while len(seq) < quantity:
-            num = random.randint(min, max)
+            num = random.randrange(min, max + 1)
             seq.add(num)
         
         result = sorted(list(seq))
     return result
+
 
 
